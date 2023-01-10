c98644535abb5b0273fbcd13692a758612621704
Isaad Khan <IKhan@lcs.on.ca>
2023-01-10 15:26:51 -0500

    Initial Commit

---
+++ LayoutExcercise/LayoutExcerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutExcerciseApp.swift
+//  LayoutExcercise
+//
+//  Created by Isaad Khan on 2023-01-10.
+//
+
+import SwiftUI
+
+@main
+struct LayoutExcerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
